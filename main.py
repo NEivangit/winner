70ec358b8f0b0f844ea84dce1a59907fc737bf5d
NEivan <ivankat19052014@gmail.com>
2024-08-15 19:21:03 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QLabel,QVBoxLayout
+import random
+app = QApplication([])
+window = QWidget()
+window.show()
+window.setWindowTitle("DJ Arbus")
+push = qPushButton('Winner')
+Text = QLabel('?')
+Text2 = QLabel('Press to calcylaite')
+Line =QVBoxLayout
+Line.addWidget(Text2,aligment=Qt.AllignHCenter)
+Line.addWidget(Text,aligment=Qt.AllignHCenter)
+Line.addWidget(push,aligment=Qt.AllignHCenter)
+window.setLayout(Line)
+def pushbutton():
+    number = random.randint(0, 100)
+    text.setText(str(number))
+    push.clicked.connect(pushbutton)
+
+app.exec_()
